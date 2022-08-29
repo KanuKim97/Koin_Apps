bc1a341c8a8aa36bcb7f9f528dd34b521ef57df5
KanuKim97 <74421057+KanuKim97@users.noreply.github.com>
2022-08-29 22:50:50 +0900

    add RecyclerView.kt

---
+++ app/src/main/java/com/example/koin_apps/data/recyclerViewAdapter/RecyclerViewAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.koin_apps.data.recyclerViewAdapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.koin_apps.R
+
+class RecyclerViewAdapter(private val coinTitleSet: Set<String?>?)
+    : RecyclerView.Adapter<RecyclerViewAdapter.CoinTitleViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoinTitleViewHolder {
+        val itemView = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.titlecoinlist, parent, false)
+
+        return CoinTitleViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: CoinTitleViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        return coinTitleSet?.size!!
+    }
+
+    class CoinTitleViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
+    }
+}
