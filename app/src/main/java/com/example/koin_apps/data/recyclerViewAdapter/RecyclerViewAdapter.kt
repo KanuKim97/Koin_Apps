ccaa3467167a4fc359a94db8e6cce64ad29013dd
KanuKim97 <74421057+KanuKim97@users.noreply.github.com>
2022-08-30 16:14:51 +0900

    Update RecyclerViewAdapter.kt

--- app/src/main/java/com/example/koin_apps/data/recyclerViewAdapter/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/koin_apps/data/recyclerViewAdapter/RecyclerViewAdapter.kt
@@ -3,6 +3,8 @@ package com.example.koin_apps.data.recyclerViewAdapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.koin_apps.R
 
@@ -18,6 +20,8 @@ class RecyclerViewAdapter(private val coinTitleSet: Set<String?>?)
     }
 
     override fun onBindViewHolder(holder: CoinTitleViewHolder, position: Int) {
+        val coinTitleList: Set<String?>? = coinTitleSet
+
 
     }
 
@@ -26,6 +30,7 @@ class RecyclerViewAdapter(private val coinTitleSet: Set<String?>?)
     }
 
     class CoinTitleViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-
+        val checkList: CheckBox = itemView.findViewById(R.id.checkCoin)
+        val coinTitle: TextView = itemView.findViewById(R.id.title_Coin)
     }
 }
