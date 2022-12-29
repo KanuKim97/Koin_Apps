ed1f333ec6e385fdd3d4a19531ebf82dc5be51d7
KanuKim97 <74421057+KanuKim97@users.noreply.github.com>
2022-12-30 00:26:53 +0900

    Mod LiveTimeViewModel Coroutine

--- app/src/main/java/com/example/koin_apps/LiveTimeActivity.kt
+++ app/src/main/java/com/example/koin_apps/LiveTimeActivity.kt
@@ -2,13 +2,18 @@ package com.example.koin_apps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.example.koin_apps.data.AppRepository
 import com.example.koin_apps.data.database.RoomRepo
 import com.example.koin_apps.databinding.ActivityLiveTimeBinding
 import com.example.koin_apps.viewModel.ViewModelFactory
 import com.example.koin_apps.viewModel.activity.LiveTimeViewModel
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class LiveTimeActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var liveTimeBinding: ActivityLiveTimeBinding
@@ -27,13 +32,18 @@ class LiveTimeActivity : AppCompatActivity(), View.OnClickListener {
     override fun onResume() {
         super.onResume()
         val coinTitle = intent.getStringExtra("coinTitle")
-
         liveTimeBinding.coinTitle.text = coinTitle
+
+        liveTimeViewModel.getTickerLive(coinTitle.toString())
+
         liveTimeBinding.BtnTransaction.setOnClickListener(this)
         liveTimeBinding.BtnOrderBook.setOnClickListener(this)
         liveTimeBinding.BtnCommunity.setOnClickListener(this)
 
-        supportFragmentManager.beginTransaction()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.FragmentLayout, TransactionFragment())
+            .commit()
     }
 
     override fun onClick(v: View?) {
--- app/src/main/java/com/example/koin_apps/viewModel/activity/LiveTimeViewModel.kt
+++ app/src/main/java/com/example/koin_apps/viewModel/activity/LiveTimeViewModel.kt
@@ -6,52 +6,30 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.koin_apps.data.AppRepository
-import com.example.koin_apps.data.database.tables.CoinEntity
-import com.example.koin_apps.data.remote.model.transaction.TransactionList
-import com.example.koin_apps.data.remote.model.transaction.TransactionRoot
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import org.json.JSONException
-import org.json.JSONObject
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.example.koin_apps.data.remote.model.ticker.TickerData
+import kotlinx.coroutines.*
+
 
 class LiveTimeViewModel(private val repos: AppRepository): ViewModel() {
-    private val _transactionLiveData = MutableLiveData<List<TransactionList>>()
+    private val _tickerLiveViewData = MutableLiveData<List<TickerData>>()
 
-    val transactionLiveData: LiveData<List<TransactionList>>
-        get() = _transactionLiveData
+    val tickerLiveViewData: LiveData<List<TickerData>>
+        get() = _tickerLiveViewData
 
-    fun getTickerTransaction(path: String, count: Int) {
-        viewModelScope.launch (Dispatchers.IO) {
-            val response = repos.getTransaction(path, count)
-
-            when (response.code()) {
-                200 -> {
-                    try {
-                        val responseBody = response.body()
-
-                        if (responseBody != null) {
-                            //TODO: Implementation Transaction response Data
-                        }
-                    } catch (e: NullPointerException) {
-                        throw NullPointerException("Response Data is Null or Empty")
-                    }
-                }
-                400 -> {
-                    val errJsonObj: JSONObject
-
-                    try{
-                        errJsonObj = JSONObject(response.errorBody()?.string()!!)
-                        val responseErrCode = errJsonObj.getString("status")
-                        val responseErrMsg = errJsonObj.getString("message")
-
-                        Log.d("400 Error", "$responseErrCode: $responseErrMsg")
-                    } catch (e: JSONException) { e.printStackTrace() }
-                }
+    fun getTickerLive(path: String) {
+        val tickerResponse = viewModelScope.launch {
+            while (true) {
+                delay(3000L)
+                Log.d("Coroutine","TickerResponse is still running")
             }
         }
+
+        tickerResponse.start()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelScope.cancel()
     }
 
 }
