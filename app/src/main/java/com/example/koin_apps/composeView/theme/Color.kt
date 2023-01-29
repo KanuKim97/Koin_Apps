904140db5d0874213c88deee0590e9fd381bc110
KanuKim97 <74421057+KanuKim97@users.noreply.github.com>
2023-01-29 23:41:49 +0900

    init commit
    Add ComposeView Package
    Add Color.kt, Shape.kt, Theme.kt, Type.kt
    Problem Founded -> DuplicateClass Build Error

---
+++ app/src/main/java/com/example/koin_apps/composeView/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.example.koin_apps.composeView.theme
+import androidx.compose.ui.graphics.Color
+
+val Blue100 = Color(0xFF88C6D7)
+val Green100 = Color(0xFF90DAD3)
+val Green300 = Color(0xFFB9E3AB)
+val Mint100 = Color(0xFFD1F0E5)
+val LightViolet100 = Color(0xFFD8D1F0)
+val LightPink200 = Color(0xFFE8CDEE)
+
+val Red = Color(0xFFFF0000)
+val Blue = Color(0xFF0080FF)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
