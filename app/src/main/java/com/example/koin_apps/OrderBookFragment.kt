e1c4112408487388f8dec6f473a06d22a1e59e1f
KanuKim97 <74421057+KanuKim97@users.noreply.github.com>
2023-02-01 22:11:13 +0900

    Mod OrderBookViewModel
    Mod OrderBookViewModel.kt

--- app/src/main/java/com/example/koin_apps/OrderBookFragment.kt
+++ app/src/main/java/com/example/koin_apps/OrderBookFragment.kt
@@ -1,6 +1,7 @@
 package com.example.koin_apps
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.example.koin_apps.data.listViewAdapter.OrderBookAskListAdapter
 import com.example.koin_apps.data.listViewAdapter.OrderBookBidListAdapter
+import com.example.koin_apps.data.listViewAdapter.TransactionListAdapter
 import com.example.koin_apps.databinding.FragmentOrderBookBinding
 import com.example.koin_apps.viewModel.fragment.OrderBookViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,7 +21,7 @@ class OrderBookFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val coinTitle = arguments?.getString("coinTitle")
-
+        orderBookViewModel.setCurrencyTitle(coinTitle.toString())
     }
 
     override fun onCreateView(
@@ -28,7 +30,24 @@ class OrderBookFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         orderBookBinding = FragmentOrderBookBinding.inflate(inflater, container, false)
+
+
+        orderBookViewModel.tickerLiveData.observe(viewLifecycleOwner) {
+            orderBookBinding.orderBookTickerInfo.text =
+                getString(
+                    R.string.orderBook_Ticker_Info,
+                    it.closingPrice,
+                    it.prevClosingPrice,
+                    it.maxPrice,
+                    it.minPrice,
+                    it.unitsTraded
+                )
+        }
+
+        orderBookViewModel.transactionLiveData.observe(viewLifecycleOwner) {
+            orderBookBinding.transactionList.adapter = TransactionListAdapter(it)
+        }
+
         return orderBookBinding.root
     }
-
 }
--- app/src/main/java/com/example/koin_apps/viewModel/fragment/OrderBookViewModel.kt
+++ app/src/main/java/com/example/koin_apps/viewModel/fragment/OrderBookViewModel.kt
@@ -1,37 +1,49 @@
 package com.example.koin_apps.viewModel.fragment
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.koin_apps.data.di.AppRepository
 import com.example.koin_apps.data.remote.model.orderBook.OrderData
-import com.example.koin_apps.data.remote.model.transaction.TransactionRoot
+import com.example.koin_apps.data.remote.model.ticker.OrderTickerData
+import com.example.koin_apps.data.remote.model.transaction.TransactionData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import retrofit2.Response
 import javax.inject.Inject
 
 @HiltViewModel
 class OrderBookViewModel @Inject constructor(
     private val repos: AppRepository
 ): ViewModel() {
+    private val _transactionLiveData = MutableLiveData<ArrayList<TransactionData>>()
     private val _orderBookLiveData = MutableLiveData<OrderData>()
+    private val _tickerLiveData = MutableLiveData<OrderTickerData>()
+
+    val transactionLiveData: LiveData<ArrayList<TransactionData>>
+        get() = _transactionLiveData
     val orderBookLiveData: LiveData<OrderData>
         get() = _orderBookLiveData
+    val tickerLiveData: LiveData<OrderTickerData>
+        get() = _tickerLiveData
+
+    private var coinTitle: String = ""
 
     private val tickerJob: Job = viewModelScope.launch {
         while (true) {
-            val response = repos.getTicker("Btc")
+            val response = repos.getTicker(coinTitle)
             if (response.isSuccessful && response.body() != null) {
-                response.body()!!.data.get("closing_price")
-                response.body()!!.data.get("prev_closing_price")
-                response.body()!!.data.get("max_price")
-                response.body()!!.data.get("min_price")
-                response.body()!!.data.get("units_traded_24H")
+                _tickerLiveData.postValue(
+                    OrderTickerData(response.body()!!.data["closing_price"].toString(),
+                        response.body()!!.data["prev_closing_price"].toString(),
+                        response.body()!!.data["max_price"].toString(),
+                        response.body()!!.data["min_price"].toString(),
+                        response.body()!!.data["units_traded_24H"].toString()
+                    )
+                )
             }
             delay(1000L)
         }
@@ -39,22 +51,35 @@ class OrderBookViewModel @Inject constructor(
 
     private val transactionJob: Job = viewModelScope.launch {
         while (true) {
-            val response = repos.getTransaction("BTC", 10)
+            val response = repos.getTransaction(coinTitle, 10)
             if (response.isSuccessful && response.body() != null) {
-                response.body()!!.data
+                _transactionLiveData.postValue(response.body()!!.data)
             }
+            delay(1000L)
         }
     }
 
     private val orderBookJob: Job = viewModelScope.launch {
         while (true) {
-            val response = repos.getOrderBook("BTC", 10)
+            val response = repos.getOrderBook(coinTitle, 10)
             if (response.isSuccessful && response.body() != null) {
                 _orderBookLiveData.postValue(response.body()!!.data)
             }
+            delay(1000L)
         }
     }
-    
+
+    fun setCurrencyTitle(path: String) {
+        coinTitle = path
+        if (coinTitle.isNotEmpty()) { startAPICallJob() }
+    }
+
+    private fun startAPICallJob() {
+        tickerJob.start()
+        transactionJob.start()
+        orderBookJob.start()
+    }
+
     override fun onCleared() {
         super.onCleared()
         tickerJob.cancel()
