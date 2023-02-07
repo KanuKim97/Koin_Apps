fb1dab822b1d3f7ab22b4d3254a7d9063117c44c
KanuKim97 <74421057+KanuKim97@users.noreply.github.com>
2023-02-07 17:54:39 +0900

    separate Modules, add Dispatcher Module
    Add Dispatcher Module
    Separate AppModule to Api, DB Module

--- app/src/main/java/com/example/koin_apps/viewModel/activity/LiveTimeViewModel.kt
+++ app/src/main/java/com/example/koin_apps/viewModel/activity/LiveTimeViewModel.kt
@@ -4,15 +4,15 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.koin_apps.data.di.repository.ApiRepository
 import kotlinx.coroutines.*
-import com.example.koin_apps.data.di.AppRepository
 import com.example.koin_apps.data.remote.model.ticker.LiveTickerData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class LiveTimeViewModel @Inject constructor(
-    private val repos: AppRepository
+    private val bithumbApiRepos: ApiRepository
 ): ViewModel() {
     private val _tickerLiveViewData = MutableLiveData<LiveTickerData>()
     val tickerLiveViewData: LiveData<LiveTickerData>
@@ -21,7 +21,7 @@ class LiveTimeViewModel @Inject constructor(
     private var path: String = ""
     private val tickerLiveJob: Job = viewModelScope.launch {
         while (true) {
-            val response = repos.getTicker(path = path)
+            val response = bithumbApiRepos.getTicker(path = path)
             if (response.isSuccessful && response.code() == 200) {
                 val resultData = LiveTickerData(
                     response.body()?.data?.get("closing_price").toString(),
@@ -45,5 +45,4 @@ class LiveTimeViewModel @Inject constructor(
         super.onCleared()
         tickerLiveJob.cancel()
     }
-
 }
