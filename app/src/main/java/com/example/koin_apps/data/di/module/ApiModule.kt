dd79444e8e517d4283c46fe8022ad6941137162a
KanuKim97 <74421057+KanuKim97@users.noreply.github.com>
2023-03-14 23:20:16 +0900

    mod code

--- app/src/main/java/com/example/koin_apps/data/di/module/ApiModule.kt
+++ app/src/main/java/com/example/koin_apps/data/di/module/ApiModule.kt
@@ -17,7 +17,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object ApiModule {
-    /* Create Retrofit Instance */
     @Provides
     fun providesBaseUrl(): String = Constants.IKoinPublicApiUri
 
