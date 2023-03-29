55ec55f7e05ca004189e59396f5e4d3faf07e62b
KanuKim97 <74421057+KanuKim97@users.noreply.github.com>
2023-03-29 19:28:44 +0900

    Add formatter
    add wonFormat
    add unitsFormat

--- app/src/main/java/com/example/koin_apps/data/di/repository/CoinRepository.kt
+++ app/src/main/java/com/example/koin_apps/data/di/repository/CoinRepository.kt
@@ -1,5 +1,6 @@
 package com.example.koin_apps.data.di.repository
 
+import android.icu.text.DecimalFormat
 import com.example.koin_apps.data.di.dataSource.CoinRemoteDataSource
 import com.example.koin_apps.data.remote.model.ticker.LiveTickerData
 import com.example.koin_apps.data.remote.model.ticker.OrderTickerData
@@ -10,25 +11,28 @@ import javax.inject.Inject
 class CoinRepository @Inject constructor(
     private val coinRemoteDataSource: CoinRemoteDataSource
 ) {
+    private val wonFormat = DecimalFormat("###,###")
+    private val unitsFormat = DecimalFormat("##.##")
+
     fun getTickerInfoALL() = coinRemoteDataSource.getTickerInfoAll()
 
     fun getTickerInfoLive(ticker: String) = coinRemoteDataSource.getTickerInfo(ticker)
         .map { result ->
             LiveTickerData(
-                result?.get("closing_price").toString(),
-                result?.get("fluctate_24H").toString(),
-                result?.get("fluctate_rate_24H").toString()
+                wonFormat.format(result?.get("closing_price").toString().toInt()),
+                wonFormat.format(result?.get("fluctate_24H").toString().toInt()),
+                unitsFormat.format(result?.get("fluctate_rate_24H").toString().toDouble())
             )
         }
 
     fun getTickerInfoDetail(ticker: String) = coinRemoteDataSource.getTickerInfo(ticker)
         .map { result ->
             OrderTickerData(
-                result?.get("closing_price").toString(),
-                result?.get("prev_closing_price").toString(),
-                result?.get("max_price").toString(),
-                result?.get("min_price").toString(),
-                result?.get("units_traded_24H").toString()
+                wonFormat.format(result?.get("closing_price").toString().toInt()),
+                wonFormat.format(result?.get("prev_closing_price").toString().toInt()),
+                wonFormat.format(result?.get("max_price").toString().toInt()),
+                wonFormat.format(result?.get("min_price").toString().toInt()),
+                unitsFormat.format(result?.get("units_traded_24H").toString().toDouble())
             )
         }
 
