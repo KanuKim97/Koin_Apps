30f2645fffc7a600d0ece810e432b42d299237dc
kanukim97 <angry_ryan@kakao.com>
2023-11-09 00:32:17 +0900

    [Modified Repository]  - Modify BithumbApiRepository  - Modify BithumbApiRepositoryImpl

--- domain/src/main/java/com/example/domain/repository/BithumbApiRepository.kt
+++ domain/src/main/java/com/example/domain/repository/BithumbApiRepository.kt
@@ -1,16 +1,11 @@
 package com.example.domain.repository
 
-import com.example.domain.entity.api.orderBook.OrderBookEntity
-import com.example.domain.entity.api.ticker.TickerInfoDetailEntity
-import com.example.domain.entity.api.transaction.TransactionEntity
-import kotlinx.coroutines.flow.Flow
-
 interface BithumbApiRepository {
-    fun getTickerInfoAll(): Flow<MutableList<String>?>
+    fun getTickerInfoAll()
 
-    fun getTickerInfo(ticker: String): Flow<TickerInfoDetailEntity?>
+    fun getTickerInfo(ticker: String)
 
-    fun getTransactionHistory(ticker: String, count: Int): Flow<List<TransactionEntity>?>
+    fun getTransactionHistory(ticker: String, count: Int)
 
-    fun getOrderBookInfo(ticker: String, count: Int): Flow<OrderBookEntity?>
+    fun getOrderBookInfo(ticker: String, count: Int)
 }
